33cf73a46afda4aae6d1e313bab8a2f4f97aaf0e
RahulChoudhary04 <rahulchoudhary4787@gmail.com>
2025-09-04 17:50:53 +0530

    add comment to db\index.js file

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,22 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
+import mongoose from "mongoose"; // Import the mongoose library to interact with MongoDB
+import { DB_NAME } from "../constants.js"; // Import the database name constant from the constants file
 
 
+// Define an asynchronous function to connect to the MongoDB database
 const connectDB = async () => {
     try {
+        // Try to connect to MongoDB using the URI from environment variables and the database name
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        // If successful, log the host of the connected database
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
         
     } catch (error) {
+        // If there is an error during connection, log the error message
         console.log("MongoDB connection error ", error);
+        // Exit the process with a failure code
         process.exit(1);
     }
 }
 
+// Export the connectDB function so it can be used in other files
 export default connectDB
