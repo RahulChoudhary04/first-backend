f437a662f6c86ae8d7a1b9d40094abccad2ae56a
RahulChoudhary04 <rahulchoudhary4787@gmail.com>
2025-09-07 11:07:45 +0530

    add steps to register user in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,17 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async (req, res) => {
-    return res.status(200).json({
-        message: "OK"
-    })
+    // get user details from frontend 
+    // validation lagana padega to check not empty
+    // check if user already exists
+    // files hai ya nhi , eg. avatar and image le rhe hai
+    // upload them to cloudinary, check avatar iploaded or not as it is compulsory
+    // create user object - create entry in db
+    // remove password and refresh token from response
+    // check if user created or not
+    // return response
+
+
 })
 
 
