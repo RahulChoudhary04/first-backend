9dc7747fb598086413b20d084a5c0a6c040dac44
RahulChoudhary04 <rahulchoudhary4787@gmail.com>
2025-09-13 21:16:36 +0530

    initial commit

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -13,7 +13,7 @@ export const verifyJWT = asyncHandler (async (req, _, next) => {
         }
         // decode token
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-    
+        // find user by id from decoded token
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
     
         if(!user){
