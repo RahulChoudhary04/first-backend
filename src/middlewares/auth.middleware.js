11f836665d2bdcdf2dcf239ea6aae73009aebd25
RahulChoudhary04 <rahulchoudhary4787@gmail.com>
2025-09-13 21:15:30 +0530

    initial commit

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,7 +7,7 @@ import { User } from "../models/user.model.js";
 export const verifyJWT = asyncHandler (async (req, _, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-    
+        // if no token then throw error
         if(!token){
             throw new ApiError(401, "Unauthorized request")
         }
