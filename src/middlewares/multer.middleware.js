33afbb878fd00abc93de7d3ec420e219bce332cd
RahulChoudhary04 <rahulchoudhary4787@gmail.com>
2025-09-14 22:56:33 +0530

    initial commit

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,6 +1,7 @@
 // Multer middleware for handling file uploads, storing files in './public/temp' with original names.
 import multer from "multer";
 
+// Set up storage engine to store files in './public/temp' with original names
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/temp")
